6d6467f2d80b72c4dbaa7db924d50f197020f074
Sodiq179 <babawalesodiq996@gmail.com>
2022-03-12 06:11:17 -0800

    101-quote.c script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(void)
+int main(void/*function tht print with write function*/)
 {
-	/*function that use write to print*/
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
