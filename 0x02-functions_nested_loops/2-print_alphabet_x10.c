895364e0cb91207c7b54b1cfe3cfb5615fe45ccb
Sodiq179 <babawalesodiq996@gmail.com>
2022-03-16 10:58:10 -0700

    file update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/*
- * A function that prints 10 times the alphabet, in lowercase, followed by a new line.
+/**
+ * print_alphabet_x10 - A function that prints 10 times the alphabet, in lowercase, followed by a new line.
  */
 
 void print_alphabet_x10(void)
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _islower - this function returns 1 if c is a lowercase alphabet else return 0
+ * @c: The alphabet to be checked
+ *
+ * Return: it returns either 1 or 0
+ */
 int _islower(int c)
 {
 	if (c>='a' && c<='z'){
