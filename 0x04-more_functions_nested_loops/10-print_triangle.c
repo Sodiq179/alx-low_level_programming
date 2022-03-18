ab6f6e598cc5bb2761dfd2b23ef266562991567b
Sodiq179 <babawalesodiq996@gmail.com>
2022-03-18 15:17:20 -0700

    10-print_triangle.c file update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,12 +12,15 @@
 void print_triangle(int size)
 {
 	int height;
+
 	int width;
+
 	int draw;
 
 	if (size <= 0)
+	{
 		_putchar('\n');
-
+	}
 	for (height = 1; height <= size; height++)
 	{
 		for (width = 1; width <= (size - height); width++)
