3e1eff4a9ce3c21e57ea85c9a63fde1e514525c6
Sodiq179 <babawalesodiq996@gmail.com>
2022-03-29 12:49:52 -0700

    Task 0 file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memeset - This function fills memory with a constant byte
+ * @s: The array to be modified
+ * @b: The value of which the array is modified with
+ * @n: Number of bit to be modified
+ *
+ * Return: Pointer to the array
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    char *point[n];
+    for (int i = 0; i < n; i++){
+        point[i] = &s[i];
+    }
+    for (int i = 0; i < n; i++){
+        *point[i] = b;
+    }
+    return (point);
+}
