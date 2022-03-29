a2438b548e701d4b367b78753a4f8a208ad6812d
Sodiq179 <babawalesodiq996@gmail.com>
2022-03-29 13:31:06 -0700

    Task_1 file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,9 +10,16 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    for (int i = 0; i < n; i++)
-    {
-        dest[i] = src[i];
-    }
-    return (dest);
+
+	int size = n; /* only accept positive sizes */
+
+	if (size > 0)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+			dest[i] = src[i];
+	}
+
+	return (dest);
 }
