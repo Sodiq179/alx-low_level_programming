1e451355bde046e944b3335d1bac557ba490d37d
Sodiq179 <babawalesodiq996@gmail.com>
2022-03-30 12:19:25 -0700

    Task 0 program file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strlen - To get the length of a string 
+ * @str: The string
+ * 
+ * Return: the length of the string
+ */
+unsigned int _strlen(char *str)
+{
+    int len = 0;
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        len++;
+    }
+    return (len);
+}
+
+/**
+ * _puts_recursion - This function prints out a string 
+ * @s: The string
+ */
+void _puts_recursion(char *s)
+{
+    for (int i = 0; i < _strlen(s); i++)
+    {
+        _putchar(s[i]);
+    }
+    _putchar('\n');
+}
