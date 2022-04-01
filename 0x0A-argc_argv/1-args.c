287b47ccd53b17f7ba26ed8dcbfe900cc5a51bde
Sodiq179 <babawalesodiq996@gmail.com>
2022-04-01 11:30:27 -0700

    Task 1 c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print name of program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
