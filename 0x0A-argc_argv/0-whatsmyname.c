2a10fa7122c3f06078026f5c821d4a2a59e2993f
Sodiq179 <babawalesodiq996@gmail.com>
2022-04-01 10:54:05 -0700

    Task 0 c file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - prints out the file name
+ * @argc: the argument count
+ * @argv: the argument vectors
+ *
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	printf("%s\n",argv[0]);
