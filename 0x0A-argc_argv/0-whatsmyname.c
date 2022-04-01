61ef12f5f53db1ba5d1ee61b6d12249229f9931a
Sodiq179 <babawalesodiq996@gmail.com>
2022-04-01 11:06:47 -0700

    Task 0 c file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,6 +11,10 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n",argv[0]);
+	if (argc > 0)
+	{
+		printf("%s\n",argv[0]);
+	}
+
 	return (0);
 }
