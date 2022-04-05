c48ff37da9967b1091ff7667b9d84fc0a57c6c64
Sodiq179 <babawalesodiq996@gmail.com>
2022-04-05 09:23:12 -0700

    Array creation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,10 +19,16 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+	{
+		retturn (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		s[i] = c;
 
 	}
+	s[i] = '\0';
 	return (s);
 }
