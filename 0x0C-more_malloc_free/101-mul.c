9520f2ac28d182fec17f3103c375008e42017860
Sodiq179 <babawalesodiq996@gmail.com>
2022-04-09 10:17:30 -0700

    Task 5 updated

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,28 +6,17 @@
  * @argv: args
  * Return: int
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv)
 {
-	unsigned long mul;
-	int i, j;
+    if (argc < 3){
+        printf("Error\n");
+        exit(98);
+    }
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-			exit(98);
-			}
-		}
-	}
-	mul = atoi(argv[1]) *  atoi(argv[2]);
-	printf("%lu\n", mul);
-	return (0);
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+    int ans = num1 * num2;
+
+    printf("%d\n", ans);
+    return(0);
 }
