0bb8c9361de9c1e131a65ea899fc500ac374d74e
Sodiq179 <babawalesodiq996@gmail.com>
2022-04-28 07:38:04 -0700

    file update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,68 +1,37 @@
 #include "main.h"
 
 /**
- * raise_to - raise an integer to the power of another
- * @x: The valu to be raised
- * @y: The power to be raised to
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
  *
- * Return: The result of the operation
- */
-int raise_to(int x, unsigned int y)
-{
-	int result = 1;
-	unsigned int i = 0;
-
-	for (i = 0; i < y; i++)
-	{
-		result *= x;
-	}
-	return (result);
-}
-
-/**
- * _strlen - calculate the length of a string
- * @s: The string
- *
- * Return: The length of the string
- */
-int _strlen(const char *s)
-{
-	int len = 0, i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		len++;
-	}
-	return (len);
-}
-
-/**
- * binary_to_uint - converts binary to an unsigned int
- * @b: The binary to be converted
- *
- * Return: The value in unsigned int
+ * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0, j = 0;
+	unsigned int ui;
+	int len, base_two;
 
 	if (!b)
-	{
 		return (0);
-	}
-	int b_size = _strlen(b);
-	unsigned int int_val = 0;
 
-	for (i = 0; i < b_size; i++)
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (!(b[i] == '0' || b[i] == '1'))
+		if (b[len] != '0' && b[len] != '1')
 		{
 			return (0);
 		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	for (i = b_size - 1, j = 0; i >= 0; i--, j++)
-	{
-		int_val += ((b[i] - 48) * raise_to(2, j));
-	}
-	return (int_val);
+
+	return (ui);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-int raise_to(int x, unsigned int y);
-int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
