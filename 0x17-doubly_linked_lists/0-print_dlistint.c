3605502aa7c6f8444abf658e9da30b91e43cd1ba
Sodiq179 <babawalesodiq996@gmail.com>
2022-06-09 13:37:33 -0700

    [Mandatory Tasks] Initial commit

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all data of doubly linked list
+ * @h: pointer to head of list
+ * Return: number of elements
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t num_nodes = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num_nodes += 1;
+	}
+
+	return (num_nodes);
+}
